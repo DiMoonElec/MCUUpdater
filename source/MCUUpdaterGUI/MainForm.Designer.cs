7b6dd52f26e0a62339e9d9ffb6f5bd2d88bf5d90
DiMoon Electronics <dmitri256s@yandex.ru>
2026-01-30 15:37:34 +0300

    feat: add app ico

--- source/MCUUpdaterGUI/MainForm.Designer.cs
+++ source/MCUUpdaterGUI/MainForm.Designer.cs
@@ -28,6 +28,7 @@
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
       this.groupboxConnection = new System.Windows.Forms.GroupBox();
       this.buttonAbort = new System.Windows.Forms.Button();
       this.buttonStartUpdate = new System.Windows.Forms.Button();
@@ -49,11 +50,9 @@
       this.groupboxConnection.Controls.Add(this.buttonAbort);
       this.groupboxConnection.Controls.Add(this.buttonStartUpdate);
       this.groupboxConnection.Controls.Add(this.transportUI1);
-      this.groupboxConnection.Location = new System.Drawing.Point(16, 87);
-      this.groupboxConnection.Margin = new System.Windows.Forms.Padding(4);
+      this.groupboxConnection.Location = new System.Drawing.Point(12, 71);
       this.groupboxConnection.Name = "groupboxConnection";
-      this.groupboxConnection.Padding = new System.Windows.Forms.Padding(4);
-      this.groupboxConnection.Size = new System.Drawing.Size(697, 268);
+      this.groupboxConnection.Size = new System.Drawing.Size(523, 218);
       this.groupboxConnection.TabIndex = 2;
       this.groupboxConnection.TabStop = false;
       this.groupboxConnection.Text = "Connection";
@@ -62,10 +61,9 @@
       // 
       this.buttonAbort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
       this.buttonAbort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-      this.buttonAbort.Location = new System.Drawing.Point(516, 101);
-      this.buttonAbort.Margin = new System.Windows.Forms.Padding(4);
+      this.buttonAbort.Location = new System.Drawing.Point(387, 82);
       this.buttonAbort.Name = "buttonAbort";
-      this.buttonAbort.Size = new System.Drawing.Size(173, 74);
+      this.buttonAbort.Size = new System.Drawing.Size(130, 60);
       this.buttonAbort.TabIndex = 5;
       this.buttonAbort.Text = "ABORT";
       this.buttonAbort.UseVisualStyleBackColor = false;
@@ -75,10 +73,9 @@
       // 
       this.buttonStartUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
       this.buttonStartUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-      this.buttonStartUpdate.Location = new System.Drawing.Point(516, 20);
-      this.buttonStartUpdate.Margin = new System.Windows.Forms.Padding(4);
+      this.buttonStartUpdate.Location = new System.Drawing.Point(387, 16);
       this.buttonStartUpdate.Name = "buttonStartUpdate";
-      this.buttonStartUpdate.Size = new System.Drawing.Size(173, 74);
+      this.buttonStartUpdate.Size = new System.Drawing.Size(130, 60);
       this.buttonStartUpdate.TabIndex = 4;
       this.buttonStartUpdate.Text = "START\r\nUPDATE";
       this.buttonStartUpdate.UseVisualStyleBackColor = false;
@@ -86,31 +83,28 @@
       // 
       // transportUI1
       // 
-      this.transportUI1.Location = new System.Drawing.Point(8, 20);
-      this.transportUI1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+      this.transportUI1.Location = new System.Drawing.Point(6, 16);
+      this.transportUI1.Margin = new System.Windows.Forms.Padding(6);
       this.transportUI1.Name = "transportUI1";
-      this.transportUI1.Size = new System.Drawing.Size(500, 238);
+      this.transportUI1.Size = new System.Drawing.Size(375, 193);
       this.transportUI1.TabIndex = 1;
       // 
       // groupBox2
       // 
       this.groupBox2.Controls.Add(this.buttonSelectFile);
       this.groupBox2.Controls.Add(this.textBoxSelectedFile);
-      this.groupBox2.Location = new System.Drawing.Point(16, 15);
-      this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+      this.groupBox2.Location = new System.Drawing.Point(12, 12);
       this.groupBox2.Name = "groupBox2";
-      this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-      this.groupBox2.Size = new System.Drawing.Size(697, 65);
+      this.groupBox2.Size = new System.Drawing.Size(523, 53);
       this.groupBox2.TabIndex = 3;
       this.groupBox2.TabStop = false;
       this.groupBox2.Text = "Update file";
       // 
       // buttonSelectFile
       // 
-      this.buttonSelectFile.Location = new System.Drawing.Point(589, 21);
-      this.buttonSelectFile.Margin = new System.Windows.Forms.Padding(4);
+      this.buttonSelectFile.Location = new System.Drawing.Point(442, 17);
       this.buttonSelectFile.Name = "buttonSelectFile";
-      this.buttonSelectFile.Size = new System.Drawing.Size(100, 28);
+      this.buttonSelectFile.Size = new System.Drawing.Size(75, 23);
       this.buttonSelectFile.TabIndex = 1;
       this.buttonSelectFile.Text = "Select";
       this.buttonSelectFile.UseVisualStyleBackColor = true;
@@ -119,11 +113,10 @@
       // textBoxSelectedFile
       // 
       this.textBoxSelectedFile.BackColor = System.Drawing.Color.White;
-      this.textBoxSelectedFile.Location = new System.Drawing.Point(8, 23);
-      this.textBoxSelectedFile.Margin = new System.Windows.Forms.Padding(4);
+      this.textBoxSelectedFile.Location = new System.Drawing.Point(6, 19);
       this.textBoxSelectedFile.Name = "textBoxSelectedFile";
       this.textBoxSelectedFile.ReadOnly = true;
-      this.textBoxSelectedFile.Size = new System.Drawing.Size(572, 22);
+      this.textBoxSelectedFile.Size = new System.Drawing.Size(430, 20);
       this.textBoxSelectedFile.TabIndex = 0;
       // 
       // statusStrip1
@@ -132,10 +125,9 @@
       this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.labelProgressBar,
             this.progressBarProgress});
-      this.statusStrip1.Location = new System.Drawing.Point(0, 580);
+      this.statusStrip1.Location = new System.Drawing.Point(0, 470);
       this.statusStrip1.Name = "statusStrip1";
-      this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
-      this.statusStrip1.Size = new System.Drawing.Size(729, 28);
+      this.statusStrip1.Size = new System.Drawing.Size(547, 24);
       this.statusStrip1.SizingGrip = false;
       this.statusStrip1.TabIndex = 4;
       this.statusStrip1.Text = "statusStrip1";
@@ -144,14 +136,14 @@
       // 
       this.labelProgressBar.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
       this.labelProgressBar.Name = "labelProgressBar";
-      this.labelProgressBar.Size = new System.Drawing.Size(48, 22);
+      this.labelProgressBar.Size = new System.Drawing.Size(42, 19);
       this.labelProgressBar.Text = "Ready";
       this.labelProgressBar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
       // 
       // progressBarProgress
       // 
       this.progressBarProgress.Name = "progressBarProgress";
-      this.progressBarProgress.Size = new System.Drawing.Size(267, 20);
+      this.progressBarProgress.Size = new System.Drawing.Size(200, 18);
       // 
       // logRichTextBox
       // 
@@ -161,13 +153,12 @@
       this.logRichTextBox.ErrorColor = System.Drawing.Color.DarkRed;
       this.logRichTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
       this.logRichTextBox.InfoColor = System.Drawing.Color.SteelBlue;
-      this.logRichTextBox.Location = new System.Drawing.Point(16, 363);
-      this.logRichTextBox.Margin = new System.Windows.Forms.Padding(4);
+      this.logRichTextBox.Location = new System.Drawing.Point(12, 295);
       this.logRichTextBox.MaxLines = 100;
       this.logRichTextBox.Name = "logRichTextBox";
       this.logRichTextBox.ReadOnly = true;
       this.logRichTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-      this.logRichTextBox.Size = new System.Drawing.Size(696, 211);
+      this.logRichTextBox.Size = new System.Drawing.Size(523, 172);
       this.logRichTextBox.TabIndex = 5;
       this.logRichTextBox.Text = "";
       this.logRichTextBox.TimestampColor = System.Drawing.Color.DarkGray;
@@ -176,15 +167,15 @@
       // 
       // MainForm
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(729, 608);
+      this.ClientSize = new System.Drawing.Size(547, 494);
       this.Controls.Add(this.logRichTextBox);
       this.Controls.Add(this.statusStrip1);
       this.Controls.Add(this.groupBox2);
       this.Controls.Add(this.groupboxConnection);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-      this.Margin = new System.Windows.Forms.Padding(4);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.MaximizeBox = false;
       this.Name = "MainForm";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
